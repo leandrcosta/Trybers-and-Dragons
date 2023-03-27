c9fbf8e6583a491efb049de98bd26db02f97eaf9
leandrcosta <leandrocostamdc@gmail.com>
2023-03-27 18:51:16 -0300

    Erro no codigo - Copiei e colei as classes mas esqueci de alterar o nome

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,13 +1,13 @@
 import Race from './Race';
 // https://www.youtube.com/watch?v=ld4QMSgfAXc
-export default class Dwarf extends Race { // Dwarf herda alguns "comportamentos" d Race
+export default class Halfling extends Race { // Dwarf herda alguns "comportamentos" d Race
   public _maxLifePoints = 60; // Pontos maximos de vida
   public static numberOfInstances = 0; // Numero de instancias( iniciei com zero)
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity); // argumentos herdados da class pai Race
 
-    Dwarf.numberOfInstances += 1; // incrementa a cada vez que uma instancia for criada
+    Halfling.numberOfInstances += 1; // incrementa a cada vez que uma instancia for criada
   }
 
   get maxLifePoints(): number { // Usando o metodo herdado de Race que tem o valor maxLife
