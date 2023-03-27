d6f9d6ccb51b6b1c521802cb31536b12207702e3
leandrcosta <leandrocostamdc@gmail.com>
2023-03-26 21:28:11 -0300

    Feat:Criando a class Race e suas especificidades

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race { // Nome Race, class abstrata
+  private readonly _name: string; // https://www.youtube.com/watch?v=Mg6FFPbG2SA
+  private readonly _dexterity: number; // privado, apenas leitura
+
+  constructor(name: string, dexterity: number) { // parametros de constructor
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string { // leitura e não pode ser alterado
+    return this._name;
+  }
+
+  public get dexterity(): number { // leitura e nao pode ser alterado
+    return this._dexterity;
+  }
+
+  public static createdRacesInstances(): number { // metodo statico e retorna um erro
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints(): number;
+}
