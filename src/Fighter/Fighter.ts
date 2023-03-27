8af687426ec6cf12a2391ba0adf5da200a8ae988
leandrcosta <leandrocostamdc@gmail.com>
2023-03-27 20:56:34 -0300

    Feat:Criação das interfaces [Fighter e SimpleFighter] e reaproveitamento de codigo ao extender a interface Fighter de SimpleFighter. Dessa forma eu consegui reaproveitar atributos e metodos em comum entre as duas interfaces

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,14 @@
+import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
+// https://www.tektutorialshub.com/typescript/optional-properties-in-typescript/
+export default interface Fighter extends SimpleFighter {
+  // lifePoints: number; atributo ja tem em simpleFighter
+  // strength: number; atributo ja tem em simpleFighter
+  defense: number;
+  energy?: Energy;
+
+  // attack(enemy: Fighter): void; reaproveitando o metodo herdado de SimpleFighter
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  // receiveDamage(attackPoints: number): number; // reaproveitando o metodo herdado de SimpleFighter
+}
