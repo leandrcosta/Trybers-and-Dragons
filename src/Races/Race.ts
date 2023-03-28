48ad9b8f688c9cd112ab991298c23ae939ab7852
leandrcosta <leandrocostamdc@gmail.com>
2023-03-28 16:04:47 -0300

    Criação da class Character e todos os seus it's. A class tem os metodos e atributos pedidos e implements da interface [Fighter]. Eu havia pulado o 7 pra fazer o 9 mas o 9 quebrava pois dependia do 7 pra passar. Então, este commit tem os requisitos 7 e 9, cm todas as suas especificidades

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,10 +1,12 @@
 export default abstract class Race { // Nome Race, class abstrata
   private readonly _name: string; // https://www.youtube.com/watch?v=Mg6FFPbG2SA
   private readonly _dexterity: number; // privado, apenas leitura
+  // private static _counterRacesInstances = 0;
 
   constructor(name: string, dexterity: number) { // parametros de constructor
     this._name = name;
     this._dexterity = dexterity;
+    // Race._counterRacesInstances += 1;
   }
 
   public get name(): string { // leitura e não pode ser alterado
